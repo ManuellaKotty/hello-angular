98e86424a9922932b5933a268a991893754f6c90
ManuellaKotty <manuellakotty@yahoo.fr>
2022-08-22 03:43:49 +0200

    appComponent: Start from blank + First component generated: FaceSnappedComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FaceSnappedComponent } from './face-snapped/face-snapped.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnappedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/face-snapped/face-snapped.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snapped',
+  templateUrl: './face-snapped.component.html',
+  styleUrls: ['./face-snapped.component.scss']
+})
+export class FaceSnappedComponent {}
