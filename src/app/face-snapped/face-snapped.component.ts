9e1a967ed22a3baee760855470a84e45d2402092
ManuellaKotty <manuellakotty@yahoo.fr>
2022-08-23 19:01:54 +0200

    First component implemented: face-snapped-component.

--- src/app/face-snapped/face-snapped.component.ts
+++ src/app/face-snapped/face-snapped.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snapped',
   templateUrl: './face-snapped.component.html',
   styleUrls: ['./face-snapped.component.scss']
 })
-export class FaceSnappedComponent {}
+export class FaceSnappedComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+
+  ngOnInit () {
+    this.title = "Bib's";
+    this.description = "Mon bébé d'amour";
+    this.createdDate = new Date();
+    this.snaps = 10;
+  }
+}
